961a0d6bf28510e2c78978d97ff0f5fd8ef06afc
XAvishek <1whitecrush@gmail.com>
2019-08-21 22:38:40 +0545

    initial

---
+++ news/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class News(models.Model):
+    CATEGORY = (("0","Politics"), ("1", "Sports"), ("2", "Fashion"), ("3", "Technology"))
+    title = models.CharField(max_length=250)
+    story = models.TextField()
+    reporter = models.ForeignKey(User, on_delete=models.SET_NULL, null = True)
+    category = models.CharField(choices=CATEGORY, max_length=2)
+    slug = models.CharField(max_length=270)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cover_image= models.ImageField(upload_to="uploads")
---
+++ news/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView, TemplateView
+from news import forms
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.utils.text import slugify
+from news.models import News
+
+from django.views import View
+
+# Create your views here.
+
+class CreateNewsView(LoginRequiredMixin, CreateView):
+    login_url = "/accounts/login/"
+    form_class = forms.CreateNewsForm
+    template_name = "news/create_news.html"
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        title = form.cleaned_data['title']
+        news = form.save(commit=False)
+        news.reporter = self.request.user
+        news.slug = slugify(title)
+        news.save()
+        return super(CreateNewsView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(CreateNewsView, self).form_invalid(form)
+
+# class NewsList(ListView):
+#     model = News
+#     context_object_name = 'news_list'
+#     template_name='index.html'
+#     ordering = ['-created_at']
+
+class NewsTemplateView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        news = News.objects.all()
+        context["latest_news"] = news.order_by("-created_at")[:10]
+        context["political_news"] = news.filter(category="0").order_by("-created_at")
+        context["sport_news"] = news.filter(category="1").order_by("-created_at")
+        context["fashion_news"] = news.filter(category="2").order_by("-created_at")
+        context["technology_news"] = news.filter(category="3").order_by("-created_at")
+        return context
+
+class NewsCategoryView(ListView):
+    model = News
+    ordering = ['-created_at']
+    context_object_name = "category_list"
+    template_name = "news/category_news.html"
+
+    def get_queryset(self):
+        print(self.kwargs)
+        category = self.kwargs.get('category')
+        category_key = [ item[0] for item in  News.CATEGORY if item[1]==category][0]
+        return News.objects.filter(category=category_key)
